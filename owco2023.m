8042b77e73047a56f7a0d032722714619d77f812
Aishwarya Mishra <45567893+Aish1606@users.noreply.github.com>
2023-02-19 16:00:16 +0530

    Add files via upload

---
+++ owco2023.m
@@ -0,0 +1,59 @@
+Ø\§A§qM<ÍGõ­XØÈN™Þ•&ð!xAPq…‰ªÑÊªzí×Ç§ðÇe;É¸WÆì—PŸÐæÉC8ƒh¥“
+T>±š
+Ej SOGEz¾Œ¢£Èãþœœ,Ö¯*ILÅ[Êl37›\jÔP“‹W|é–ÄRýhÕö«Ø¯¼+ûº¹	Xp’,Û–ñÔcÆÇ¯¸ÙÄK½È0ÔÔj
+0bEûG°ø¯Ê’TÈlÝéÍ³hA‡¥]2ÔTQl–{’îmF â²ã—ÕÒàææüD÷2ô˜¿öS8¯@âÝƒ@@`Òm\?¿øýæHv`;>¿‰˜NzôÎ
+f/¾— p2àh-5Àö}E<8m;‰
+…1ëï )ÇyÜâ‚,Ò¶GV!…úÙ¼Ü?RKÿo9As°ÿidëÎîÉüƒ7ã!A8zÊ1A2=L¹Fàçuëªteàdþ¥>ÊúæMbÂäb/õìÙ
+†uã©çRÒhœ<ÍøK«¦a<dJ$[N¡	Ê®lÕFZ#ðÆWá!¯Çîw—Ä/GèÜôð-áÕïÔ×<ìš_ÞéÅž­Ë°Íí—ìtí´ÜšßzDÝ‡w]æ#X_U=îÛÇ°g$ùtÒ˜Ñp®3Œˆ²µÀ5õöÌV3Ýæ™3¸÷Tî³Êú³Ö¿£ê“ß2;µ¬åM+{w§H…<BÒ Ö¹µ¾ 1jCÑ(jƒ“¡³È‰ž£
+ŸKLyê÷Iz‘ÞIæê®7ätÌ;Æ4Î©?¤ÆoÆF‚¹1Ãöïçˆ ŽScß—fÌ–ˆÿüNÛMZ ´G%	ù~Ç„iü°N¾áX.‚€c?¦P¤ü|°d¦6wç"gµû@³#Cú¿R&¬VàšÐ›àp#\z“s(¯E’ s<Bt<ÃSë+×ŠÓÙæ”‹¤“Ã¯¢59œ9sÇÓÎ§
+ô‹ž·£*z3UvƒÆmAöDŸ×èžÇ*1Z.sýÜ¥›¼º¦™L+à*ÕD–•/B«E¿D~7P¡–ÙvfÜøLö•]ö+Vc²ÞÊ5;cÃ3;ÈÝ“ç¦*QÇœÚû/%½ÿè­ððm»ß‹Ýgyµ,8¾ÞŒzmÝC&
+ôFð|ÂXt}9$pš!-êìr»×BöÅ|Þ«‘Üª¹áÒëPã!9þ÷º×<’Á‹rÇšƒu&No›¨WFxƒVËZyÖ¯äGÀ%~VÀùÛ>¢ 
+endstream
+endobj
+82 0 obj
+<</Type/Metadata/Subtype/XML/Length 3063>>
+stream
+<?xpacket begin="ï»¿" id="W5M0MpCehiHzreSzNTczkc9d"?><x:xmpmeta xmlns:x="adobe:ns:meta/" x:xmptk="3.1-701">
+<rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">
+<rdf:Description rdf:about=""  xmlns:pdf="http://ns.adobe.com/pdf/1.3/">
+<pdf:Producer>MicrosoftÂ® Word 2019</pdf:Producer></rdf:Description>
+<rdf:Description rdf:about=""  xmlns:dc="http://purl.org/dc/elements/1.1/">
+<dc:creator><rdf:Seq><rdf:li>Sanjeev Kumar</rdf:li></rdf:Seq></dc:creator></rdf:Description>
+<rdf:Description rdf:about=""  xmlns:xmp="http://ns.adobe.com/xap/1.0/">
+<xmp:CreatorTool>MicrosoftÂ® Word 2019</xmp:CreatorTool><xmp:CreateDate>2023-02-19T15:41:26+05:30</xmp:CreateDate><xmp:ModifyDate>2023-02-19T15:41:26+05:30</xmp:ModifyDate></rdf:Description>
+<rdf:Description rdf:about=""  xmlns:xmpMM="http://ns.adobe.com/xap/1.0/mm/">
+<xmpMM:DocumentID>uuid:B5088767-B05A-44DF-AE10-01AEA8E6A489</xmpMM:DocumentID><xmpMM:InstanceID>uuid:B5088767-B05A-44DF-AE10-01AEA8E6A489</xmpMM:InstanceID></rdf:Description>
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+</rdf:RDF></x:xmpmeta><?xpacket end="w"?>
+endstream
+endobj
+83 0 obj
+<</DisplayDocTitle true>>
+endobj
+84 0 obj
+<</Type/XRef/Size 84/W[ 1 4 2] /Root 1 0 R/Info 11 0 R/ID[<678708B55AB0DF44AE1001AEA8E6A489><678708B55AB0DF44AE1001AEA8E6A489>] /Filter/FlateDecode/Length 199>>
+stream
+xœ5ÒGVBQ Ð÷±‚~PP{ïŠ{Ù—ËpN\Š·¡+p~Ÿïj¹''É(	!FQd1ç!üòŠÏDö•('*zÕg¼£HÔê‰ü#„R\o†Üâ÷¸ÆßäCÜk¼ýWJÀ †0ŒŒ¢Œ
+ªC
+uŒcxref
+75387
+%%EOF
